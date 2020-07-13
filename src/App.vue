96a511696456a1e670b3c91e5c809d24b1d802ae
unknown <sistemas@VRE_NB_CAP1804.inacap.cl>
2020-07-13 19:36:54 -0400

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <ExtraInfo titulo="Installed CLI Plugins" v-bind:enlaces="enlaces_cli_plugins" />
+    <ExtraInfo titulo="Essential Links" v-bind:enlaces="enlaces_essential_links" />
+    <ExtraInfo titulo="Ecosystem" v-bind:enlaces="enlaces_ecosystem" />
+    <br> <br> <br>
+  </div>
+</template>
+
+<script>
+import Milista from './components/Milista.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Milista
+  },
+  data: () => {
+    return {
+      // acá están mis datos
+      enlaces__realizado: [
+        {texto: 'babel', url: 'https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel'},
+        {texto: 'eslint', url: 'https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint'}
+      ],
+      enlaces_por_hacer: [
+        {url: "https://vuejs.org", texto: 'Core Docs'},
+        {url: "https://forum.vuejs.org", texto: 'Forum'},
+        {url: "https://chat.vuejs.org", texto: 'Community Chat'},
+        {url: "https://twitter.com/vuejs", texto: 'Twitter'},
+        {url: "https://news.vuejs.org", texto: 'News'}
+      ],
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Milista.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="jumbotron" id="app">
+        <div class="card">
+            <h4>{{ titulo }}</h4>
+            <ul>
+            <li v-for="(enlace, i) in enlaces" :key="i"><a href="enlace.url">{{ enlace.texto }}</a></li>
+            </ul>
+        </div>
+    </div>
+</template>
+<script>
+
+export default{
+    name:"MiLista",
+    props: {
+    titulo: String,
+    enlaces: Array
+  }
+}
+</script>
+<style scoped>
+    .jumbotron{
+        background: url(https://giffiles.alphacoders.com/208/208893.gif ) no-repeat  center;
+        background-size: 100% ;
+        height: 45rem;
+    }
+
+    .titulo{
+        font-weight: bold;
+        padding: 20px;
+        color:white;
+        font-size: 4rem;
+        text-decoration: underline;
+    }
+
+    .titulo_cuadro, .texto_consulta{
+        font-weight: bold;
+        font-size: 1.5rem;
+        text-align: justify;
+    }
+    .card{
+        color:white;
+        display: inline-block;
+        background-color: black;
+        opacity: .8;;
+        width: 25rem;
+        border-radius: 20px;
+        border: white 5px solid;
+        padding: 2rem;
+        margin: 5rem;
+        transition: transform .2s;
+    }
+        .card:hover{
+        color:black;
+        display: inline-block;
+        background-color: white;
+        opacity: .8;;
+        width: 25rem;
+        border-radius: 20px;
+        border: black 5px solid;
+        padding: 2rem;
+        margin: 5rem;
+        transform: scale(1.2);
+    }
+
+</style>
